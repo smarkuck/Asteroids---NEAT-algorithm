89e5a23d4053122177b3ec6512ae43e0698cfe92
Damian Smarkucki <smarkuck@gmail.com>
2019-08-21 21:36:00 +0200

    added possibility to create multiple game instances for human/AI, for AI there is flag to disable rendering

--- game.py
+++ game.py
@@ -7,73 +7,98 @@ from ship import Ship
 from asteroid import Asteroid
 from collision_system import CollisionSystem
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-done = False
+class Game:
+    def __init__(self, genome, isRendered):
+        self.isRendered = isRendered
+        if genome is None:
+            self.playerIsHuman = True
+        else: self.playerIsHuman = False
 
-playerIsHuman = False
+    @classmethod
+    def forHuman(cls):
+        return cls(None, True)
 
-clock = pygame.time.Clock()
-ship = Ship(pygame.math.Vector2(SCREEN_WIDTH/2, SCREEN_HEIGHT/2), SHORT_SIDE * 0.1)
+    @classmethod
+    def forAI(cls, genome, isRendered = True):
+        return cls(genome, isRendered)
 
-bullets = []
-asteroids = []
-asteroids.append(Asteroid(pygame.math.Vector2(SCREEN_WIDTH/4, SCREEN_HEIGHT/4), 3, SHORT_SIDE * 0.1))
-asteroids.append(Asteroid(pygame.math.Vector2(0, 0), 3, SHORT_SIDE * 0.1))
-asteroids.append(Asteroid(pygame.math.Vector2(0, SCREEN_HEIGHT), 3, SHORT_SIDE * 0.1))
-asteroids.append(Asteroid(pygame.math.Vector2(SCREEN_WIDTH, 0), 3, SHORT_SIDE * 0.1))
+    def run(self):
+        if self.isRendered:
+            pygame.init()
+            screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+            clock = pygame.time.Clock()
 
-collisionSystem = CollisionSystem(ship, asteroids, bullets)
+        done = False
+
+        ship = Ship(pygame.math.Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2), SHORT_SIDE * 0.1)
+
+        bullets = []
+        asteroids = []
+        asteroids.append(Asteroid(pygame.math.Vector2(SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4), 3, SHORT_SIDE * 0.1))
+        asteroids.append(Asteroid(pygame.math.Vector2(0, 0), 3, SHORT_SIDE * 0.1))
+        asteroids.append(Asteroid(pygame.math.Vector2(0, SCREEN_HEIGHT), 3, SHORT_SIDE * 0.1))
+        asteroids.append(Asteroid(pygame.math.Vector2(SCREEN_WIDTH, 0), 3, SHORT_SIDE * 0.1))
+
+        collisionSystem = CollisionSystem(ship, asteroids, bullets)
+
+        while not done:
+            bullets[:] = [bullet for bullet in bullets if bullet.alive]
+            asteroids[:] = [asteroid for asteroid in asteroids if asteroid.alive]
+
+            if len(asteroids) == 0 or not ship.alive:
+                done = True
+
+            if self.isRendered:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        done = True
+                    if self.playerIsHuman and event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                        bullets.extend(ship.shoot())
+
+            if self.playerIsHuman:
+                pressed = pygame.key.get_pressed()
+                if pressed[pygame.K_LEFT]: ship.rotateLeft()
+                if pressed[pygame.K_RIGHT]: ship.rotateRight()
+                if pressed[pygame.K_UP]: ship.boost()
+
+            else:
+                input = []
+                input.extend(
+                    [ship.position.x / SCREEN_WIDTH, ship.position.y / SCREEN_WIDTH, ship.RADIUS / SCREEN_WIDTH])
+                for asteroid in asteroids:
+                    input.extend([asteroid.position.x / SCREEN_WIDTH, asteroid.position.y / SCREEN_WIDTH,
+                                  asteroid.RADIUS / SCREEN_WIDTH])
+
+                inputSize = len(input)
+                if inputSize < 51:
+                    toFill = 51 - inputSize
+                    for i in range(toFill):
+                        input.append(0)
+
+                output = g.feedforward(input)
+
+                if output[0] > 0.5: bullets.extend(ship.shoot())
+                if output[1] > 0.5: ship.rotateLeft()
+                if output[2] > 0.5: ship.rotateRight()
+                if output[3] > 0.5: ship.boost()
+
+            ship.update()
+            for bullet in bullets: bullet.update()
+            for asteroid in asteroids: asteroid.update()
+
+            collisionSystem.checkCollisions()
+
+            if self.isRendered:
+                screen.fill(colors.black)
+                ship.draw(screen)
+                for bullet in bullets: bullet.draw(screen)
+                for asteroid in asteroids: asteroid.draw(screen)
+
+                pygame.display.flip()
+                clock.tick(60)
 
 g = Genome(51, 4)
 g.connectNodes()
 
-while not done:
-    bullets[:] = [bullet for bullet in bullets if bullet.alive]
-    asteroids[:] = [asteroid for asteroid in asteroids if asteroid.alive]
-
-    for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            if playerIsHuman and event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                bullets.extend(ship.shoot())
-
-    if playerIsHuman:
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_LEFT]: ship.rotateLeft()
-        if pressed[pygame.K_RIGHT]: ship.rotateRight()
-        if pressed[pygame.K_UP]: ship.boost()
-
-    else:
-        input = []
-        input.extend([ship.position.x/SCREEN_WIDTH, ship.position.y/SCREEN_WIDTH, ship.RADIUS/SCREEN_WIDTH])
-        for asteroid in asteroids:
-            input.extend([asteroid.position.x/SCREEN_WIDTH, asteroid.position.y/SCREEN_WIDTH, asteroid.RADIUS/SCREEN_WIDTH])
-
-        inputSize = len(input)
-        if inputSize < 51:
-            toFill = 51 - inputSize
-            for i in range(toFill):
-                input.append(0)
-
-        output = g.feedforward(input)
-
-        if output[0] > 0.5: bullets.extend(ship.shoot())
-        if output[1] > 0.5: ship.rotateLeft()
-        if output[2] > 0.5: ship.rotateRight()
-        if output[3] > 0.5: ship.boost()
-
-    ship.update()
-    for bullet in bullets: bullet.update()
-    for asteroid in asteroids: asteroid.update()
-
-    collisionSystem.checkCollisions()
-
-    screen.fill(colors.black)
-    ship.draw(screen)
-    for bullet in bullets: bullet.draw(screen)
-    for asteroid in asteroids: asteroid.draw(screen)
-
-    pygame.display.flip()
-    clock.tick(60)
-
+game = Game.forAI(g)
+game.run()
